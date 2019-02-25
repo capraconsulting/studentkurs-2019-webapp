a8ff587853422f18c4f1d62f66adc72c6b137d44
Oyvind Moldestad <moldestad@gmail.com>
2019-02-25 13:59:56 +0100

    Fix initial data

--- src/services/eventService.ts
+++ src/services/eventService.ts
@@ -8,7 +8,7 @@ let events: IEvent[] = [
     data: {
       title: 'Kurs med Abakus',
       description: 'Veldig gøy for alle involverte',
-      date: new Date('February 25, 2018'),
+      date: new Date('February 25, 2018').toISOString(),
       url:
         'https://thumbor.abakus.no/g4mD9f-JEZPPgVPqzWdrLe_xH_o=/0x500/7df72c5a291dc020b1d5_57BlAer.png'
     }
@@ -18,7 +18,7 @@ let events: IEvent[] = [
     data: {
       title: 'Mingling med Capra',
       description: 'Lorum ipsum og andre ripsbusker',
-      date: new Date('February 25, 2018'),
+      date: new Date('February 25, 2018').toISOString(),
       url:
         'https://pbs.twimg.com/profile_images/783246497450188800/jDO2Q7n5_400x400.jpg'
     }
